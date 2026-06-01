71ef752b5b80f0f6966cbfd29ec70ca0365d32a3
JaganParida <jagan.parida.dev@gmail.com>
2026-06-01 18:17:55 +0530

    chore: cleanup

--- fix-form.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('d:/Important/Development_Projects/Advanced/Gradeflow/frontend/src/pages/Home.jsx', 'utf8');
-
-// safely replace form
-content = content.replace('<form onSubmit={handleSearch}>', '<form onSubmit={handleSearch} style={{ width: \"100%\" }}>');
-
-fs.writeFileSync('d:/Important/Development_Projects/Advanced/Gradeflow/frontend/src/pages/Home.jsx', content, 'utf8');
