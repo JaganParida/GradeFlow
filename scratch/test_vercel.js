3c76493303f6d2b9d1eb7d4ba0b269684136c77a
JaganParida <jagan.parida.dev@gmail.com>
2026-08-09 01:46:31 +0530

    Clean up temporary test and scratch scripts

--- scratch/test_vercel.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.post('https://grade-flow-navy.vercel.app/api/send-backlog-email', {
-  regNo: '230301120137'
-}).then(r => console.log(r.data)).catch(e => console.error(e.response?.data || e.message));
